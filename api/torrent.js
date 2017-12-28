17ee4e5996268834ca1a42520d634d3dceff0b37
avoronov <avoronov@aviacons.ru>
2017-12-28 04:29:21 +0300

    Video Streamer, Delete torrent by id

--- api/torrent.js
+++ api/torrent.js
@@ -30,47 +30,89 @@ router.post('/', function(req, res, next) {
 
 
 router.delete('/:id', function(req, res, next) {
-    console.log("Del torrent\n");
+    console.log("Del torrent");
     console.log("Name = "+req.params.id);
-
-
-
+    Torrent.findByIdAndRemove(req.params.id, function(err, torr) {
+        if (err) {
+            console.log(err.message);
+            return next(err);
+        } else {
+            console.log(torr);
+            torr.remove();
+        }
+    });
     res.end()
 
 });
 
-router.get('/:id/file', function(req, res, next) {
+router.get('/:id/files', function(req, res, next) {
     console.log("Get files for torrent: " + req.params.id);
-
-    // var myMap = new Map();
-    // myMap.set(0, "value associated with 'a string'");
-    // myMap.set(1, "value associated with keyObj");
-    // myMap.set(2, "value associated with keyFunc");
-    // var jsonres = JSON.stringify([...myMap]);
-    //
-    //
-    // res.json(jsonres);
-    res.end()
+    var client = req.torrentClient;
+    Torrent.findOne({'_id': req.params.id}, function(err, doc) {
+        if (err) {
+            return next(err);
+        } else if (doc) {
+            console.log(doc['magnet']);
+            var torrent = client.get(doc['magnet']);
+            var files = {
+                arr: []
+            };
+            if (torrent) {
+                for (var i = 0; i < torrent.files.length; i++) {
+                    if (torrent.files[i].path ) {
+                        files.arr.push({id: i, name: torrent.files[i].path});
+                        console.log('File: ' + torrent.files[i].path);
+                    }
+                }
+                res.json(JSON.stringify(files.arr));
+            } else {
+                client.add(doc['magnet'], { store: false }, function(torrent) {
+                    console.log('Torrent is downloading: ' + torrent.infoHash);
+                    torrent._selections = [];
+                    for (var i = 0; i < torrent.files.length; i++) {
+                        if (torrent.files[i].path ) {
+                            files.arr.push({id: i, name: torrent.files[i].path});
+                            console.log('File: ' + torrent.files[i].path);
+                        }
+                    }
+                    res.json(JSON.stringify(files.arr));
+                });
+            }
+        }
+    });
 
 });
 
 
 
 router.get('/:id/files/:fid/video', function(req, res, next) {
-    console.log('begin');
+    console.log('Params id: ' + req.params.id);
     var client = req.torrentClient;
-    var torrentId = 'magnet:?' +
-        'xt=urn:btih:67EB8FCBD8D8CC08D34621649E9BC83AC669FEFE&tr=http%' +
-        '3A%2F%2Fbt4.t-ru.org%2Fann%3Fmagnet&dn=%5BiPhone%5D%20%D0%9A%' +
-        'D0%BB%D0%B8%D0%BD%D0%B8%D0%BA%D0%B0%20%2F%20Scrubs%20%2F%20%D' +
-        '0%A1%D0%B5%D0%B7%D0%BE%D0%BD%201-9%20(9)%20%2F%20%D0%A1%D0%B5' +
-        '%D1%80%D0%B8%D0%B8%201-182%20(182)%20(%D0%9C%D0%B0%D0%B9%D0%B' +
-        'A%D0%BB%20%D0%A1%D0%BF%D0%B8%D0%BB%D0%BB%D0%B5%D1%80%2C%20%D0' +
-        '%94%D0%B6%D0%BE%D0%BD%20%D0%98%D0%BD%D0%B2%D1%83%D0%B4%2C%20%' +
-        'D0%97%D0%B0%D0%BA%20%D0%91%D1%80%D0%B0%D1%84%D1%84)%20%5B2001' +
-        '-2010%2C%20%D0%B4%D1%80%D0%B0%D0%BC%D0%B0%2C%20%D0%BA%D0%BE%D' +
-        '0%BC%D0%B5%D0%B4%D0%B8%D1%8F%2C%20DVDRip%2C%20480x360%5D%20MVO%20';
-    var filePath = 'Scrubs/Season 1/1.01 - My First Day.mp4';
+    Torrent.findOne({'_id': req.params.id}, function(err, doc) {
+        if (err) {
+            return next(err);
+        } else if (doc) {
+            var torrent = client.get(doc['magnet']);
+            if (torrent) {
+                var file = torrent.files[req.params.fid];
+                console.log('File: ' + file.path);
+                startStream(file, req.headers.range);
+
+            } else {
+                client.add(doc['magnet'], { store: false }, function(torrent) {
+                    console.log('Torrent is downloading: ' + torrent.infoHash);
+                    torrent._selections = [];
+
+                    var file = torrent.files[req.params.fid];
+                    startStream(file, req.headers.range);
+
+                });
+            }
+        }
+    });
+
+
+
     var startStream = function(file, range) {
         var fileSize = file.length;
         console.log(range);
@@ -107,101 +149,8 @@ router.get('/:id/files/:fid/video', function(req, res, next) {
         }
     };
 
-    var findFile = function(torrent, filePath) {
-        for (var i = 0; i < torrent.files.length; i++) {
-            var file = torrent.files[i];
-            if (file.path === filePath) {
-                return file;
-            }
-        }
-    };
-
-    var torrent = client.get(torrentId);
-    console.log('before load');
-    if (torrent) {
-        var file = torrent.files[0];
-        startStream(file, req.headers.range);
-
-    } else {
-        client.add(torrentId, { store: false }, function(torrent) {
-            console.log('Torrent is downloading: ' + torrent.infoHash);
-            torrent._selections = [];
-
-            var file = torrent.files[0];
-            startStream(file, req.headers.range);
-
-        });
-    }
 
 });
-
-router.get('/:id/files/:fid/video/get', function(req, res) {
-    //var torrentId = req.torrentId;
-    // let's for now just say we have a parameter "torrentId"
-    var torrentId = 'magnet:?' +
-        'xt=urn:btih:67EB8FCBD8D8CC08D34621649E9BC83AC669FEFE&tr=http%' +
-        '3A%2F%2Fbt4.t-ru.org%2Fann%3Fmagnet&dn=%5BiPhone%5D%20%D0%9A%' +
-        'D0%BB%D0%B8%D0%BD%D0%B8%D0%BA%D0%B0%20%2F%20Scrubs%20%2F%20%D' +
-        '0%A1%D0%B5%D0%B7%D0%BE%D0%BD%201-9%20(9)%20%2F%20%D0%A1%D0%B5' +
-        '%D1%80%D0%B8%D0%B8%201-182%20(182)%20(%D0%9C%D0%B0%D0%B9%D0%B' +
-        'A%D0%BB%20%D0%A1%D0%BF%D0%B8%D0%BB%D0%BB%D0%B5%D1%80%2C%20%D0' +
-        '%94%D0%B6%D0%BE%D0%BD%20%D0%98%D0%BD%D0%B2%D1%83%D0%B4%2C%20%' +
-        'D0%97%D0%B0%D0%BA%20%D0%91%D1%80%D0%B0%D1%84%D1%84)%20%5B2001' +
-        '-2010%2C%20%D0%B4%D1%80%D0%B0%D0%BC%D0%B0%2C%20%D0%BA%D0%BE%D' +
-        '0%BC%D0%B5%D0%B4%D0%B8%D1%8F%2C%20DVDRip%2C%20480x360%5D%20MVO%20';
-    var filePath = 'Scrubs/Season 1/1.01 - My First Day.mp4';
-    var client = req.torrentClient;
-    var torrent = client.get(torrentId);
-    var selectedFile = null;
-    console.log('inside get');
-    if (torrent) {
-        // do stuff
-        for (var i = 0; i < torrent.files.length; i++) {
-            var file = torrent.files[i];
-            console.log(file.path);
-            if (file.path === filePath) {
-                file.select();
-                selectedFile = file;
-            }
-        }
-    }
-
-    if (selectedFile) {
-        var fileSize = selectedFile.length;
-        var range = req.headers.range;
-
-        console.log(range);
-        if (range) {
-            var parts = range.replace(/bytes=/, '').split('-');
-            var start = parseInt(parts[0], 10);
-            var end = (parts[1])? parseInt(parts[1], 10) : fileSize - 1;
-
-            var chunkSize = (end - start) + 1;
-            var headers = {
-                'Content-Range': 'bytes ' + start + ' - ' + end + '/' + chunkSize,
-                'Accept-Ranges': 'bytes',
-                'Content-Length': chunkSize,
-                'Content-Type': 'video/mp4'
-            };
-            console.log(headers);
-
-            res.writeHead(206, headers);
-            selectedFile.createReadStream({ start: start, end: end }).pipe(res);
-        } else {
-            var headers = {
-                'Content-Length': fileSize,
-                'Content-Type': 'video/mp4'
-            };
-
-            res.writeHead(200, headers);
-            selectedFile.createReadStream().pipe(res);
-        }
-    } else {
-        res.writeHead(404);
-        res.send('Not Found');
-    }
-});
-
 
 
 module.exports = router;
